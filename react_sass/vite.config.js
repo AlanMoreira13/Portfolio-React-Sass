727701cbcd839b7dbb6ca5d3b83ea135eab554af
Alan Moreira <moreiraalan22@gmail.com>
2022-07-24 14:13:02 -0300

    deploy

--- react_sass/vite.config.js
+++ react_sass/vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio-deploy/",
+  base: "/portfolio-deploy/projects",
   plugins: [react()],
 });
